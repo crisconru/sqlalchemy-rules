8afdc3d351c0b436276120a49646ab91515824b2
crisconru <crisconru@gmail.com>
2017-04-28 19:24:31 +0200

    Todo

--- fichero.py
+++ fichero.py
@@ -5,14 +5,10 @@ conector = sqlite3.connect('base_de_datos.db')
 # Crear cursor para trabajar con ella
 cursor = conector.cursor()
 # Escribir la orden -> Generar una tabla
-query = 'CREATE TABLE charla (autor VARCHAR, titulo VARCHAR)'
+query = 'CREATE TABLE IF NOT EXISTS charla (autor VARCHAR, titulo VARCHAR)'
 # Ejecutar la orden
-try:
-    cursor.execute(query)
-except sqlite3.OperationalError as e:
-    print('Error creando tabla "charla" = {}'.format(e))
-finally:
-    # Guardar la operacion
-    conector.commit()
-    # Cerrar la conexion
-    conector.close()
+cursor.execute(query)
+# Guardar la operacion
+conector.commit()
+# Cerrar la conexion
+conector.close()
